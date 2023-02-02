fbeba54db0833f4e928acad255b6736266d1be7f
jrasb <okrypczyk@protonmail.com>
2023-02-02 11:24:01 +0100

    finished opdracht 3

--- js/main.js
+++ js/main.js
@@ -14,5 +14,16 @@ const koobfaceProfile = {
     views: 'n views'
 }
 
+class Pizza {
+    constructor(name, description, ingredients) {
+        this.name = name;
+        this.description = description;
+        this.ingredients = ingredients;
+    }
+}
+
+const peperoniPizza = new Pizza('peperoni', 'mmm', 'peperoni, sauce, cheese');
+const fourCheesesPizza = new Pizza('4 cheeses', 'tasty', '4 cheeses, sauce');
+const margharitaPizza = new Pizza('margharita', 'easy peasy', 'mozzarella');
 
 /* Fancy Meow Certified -=^-^=- */
